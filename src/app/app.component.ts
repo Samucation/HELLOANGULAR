d447f8947502db5dda39fec1afe18332287b2ca6
Samuel Ferreira Duarte <samuel_fx@hotmail.com>
2018-12-22 16:12:53 -0200

    Primeiro módulo completo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'helloangular';
+  descricao = 'Esta é uma imagem muito legal, woww';
+  logo_application_thumbnail = "https://media.licdn.com/dms/image/C5603AQEveZRhGvvFBA/profile-displayphoto-shrink_200_200/0?e=1550707200&v=beta&t=gdS4NHJO0N-f145-myzvefPeT-ydQN_MnimbsbctBRE"
 }
